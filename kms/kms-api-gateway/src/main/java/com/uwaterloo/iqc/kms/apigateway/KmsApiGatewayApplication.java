7b10d5cd58c7879675ab69f52629a5fb5eea72fc
Kaiduan Xie <kaiduanx@goodstartsoft.com>
2022-06-05 15:33:03 -0400

    Use BouncyCastle.

--- kms/kms-api-gateway/src/main/java/com/uwaterloo/iqc/kms/apigateway/KmsApiGatewayApplication.java
+++ kms/kms-api-gateway/src/main/java/com/uwaterloo/iqc/kms/apigateway/KmsApiGatewayApplication.java
@@ -17,7 +17,18 @@ public class KmsApiGatewayApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(KmsApiGatewayApplication.class);
 
+    static {
+        java.security.Security.insertProviderAt(new org.bouncycastle.jce.provider.BouncyCastleProvider(), 2);
+        java.security.Security.insertProviderAt(new org.bouncycastle.jsse.provider.BouncyCastleJsseProvider(), 3);
+    }
+
     public static void main(String[] args) {
+        java.security.Provider[] providers = java.security.Security.getProviders();
+
+        for (java.security.Provider provider : providers) {
+            logger.info("Installed security providers:" + provider.getInfo() + "\n");
+        }
+
         SpringApplication.run(KmsApiGatewayApplication.class, args);
         logger.info("INFO: Starting kms-apigw ...");
     }
