6f565230529152bffdea6c1cd6dcb90ef443516e
pranshu <pranshu540@gmail.com>
2022-08-15 12:40:24 -0400

    changed DummyCheck.java to use LinkStatus for testing

--- qnl/key-routing-service/src/main/java/com/uwaterloo/iqc/qnl/DummyCheck.java
+++ qnl/key-routing-service/src/main/java/com/uwaterloo/iqc/qnl/DummyCheck.java
@@ -25,11 +25,7 @@ public class DummyCheck extends TimerTask{
         int localPort = Integer.parseInt(cfg.localSiteAgentUrl.split(":")[1]);
         String remoteAddress = cfg.remoteSiteAgentUrl.split(":")[0];
         int remotePort = Integer.parseInt(cfg.remoteSiteAgentUrl.split(":")[1]);
-        Site localSite = client.getSiteDetails(localAddress, localPort);
-        Site remoteSite = client.getSiteDetails(remoteAddress, remotePort);
-        //if(localSite.getDevicesCount() == 0)
-        LOGGER.info("The local device ID is: " + cfg.localQKDDeviceId + " number of devices on this site agent: " + localSite.getDevicesCount());
-        //if(remoteSite.getDevicesCount() == 0)
-        LOGGER.info("The remote device ID is: " + cfg.remoteQKDDeviceId + " number of devices on this site agent: " + remoteSite.getDevicesCount());
+        client.getLinkStatus(localAddress, localPort);
+        client.getLinkStatus(remoteAddress, remotePort);
     }
 }
