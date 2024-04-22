6ffc315642d7c9168cb25d8066c7770354a88369
Rahul Tandon <jesuisrahult@gmail.com>
2024-04-22 14:11:21 -0400

    Add minor error checking to run scripts.

--- kms/scripts/run-kms-service.sh
+++ kms/scripts/run-kms-service.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 touch ~/qkd_logs/kms_service.log
+JAR_FILE="kms-service/target/kms-service-0.0.1-SNAPSHOT.jar"
+if [ ! -f $JAR_FILE ]; then
+    echo "File ${JAR_FILE} not found. Please build the project first."
+    exit 1
+fi
 java -jar kms-service/target/kms-service-0.0.1-SNAPSHOT.jar > ~/qkd_logs/kms_service.log
--- kms/scripts/run-registration-service.sh
+++ kms/scripts/run-registration-service.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 touch ~/qkd_logs/registration_service.log
+JAVA_FILE="registration-service/target/registration-service-0.0.1-SNAPSHOT.jar"
+if [  ! -f $JAVA_FILE ]; then
+    echo "File ${JAVA_FILE} not found. Please build the project first."
+    exit 1
+fi
 java -jar registration-service/target/registration-service-0.0.1-SNAPSHOT.jar > ~/qkd_logs/registration_service.log
