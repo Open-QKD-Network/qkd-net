6d072b7e6dcecdc295292fb62821dbbd1eace1b9
kaiduan xie <kaiduanx@goodstartsoft.com>
2024-07-20 09:45:45 -0400

    Update libopenqkd test code.

--- applications/api/test.c
+++ applications/api/test.c
@@ -11,34 +11,38 @@ int main(int argc, char * argv[]) {
   char *key = NULL, *get_key_url = NULL;
   int len = 0;
 
-#if 0
-  /* run this on server side/bob/site B */
-  ret = oqkd_get_new_key_url(&new_key_url);
-  if (ret == 0) {
-      printf("new_key_url:%s\n", new_key_url);
-  } else {
-      printf("Fails to get new_key_url\n");
-      return ret;
+  if (argc != 3) {
+    printf("Usage:use the following two options to run\n");
+    printf("newkey newkeyurl\n");
+    printf("getkey getkeyurl\n");
+    return -1;
   }
-  ret = oqkd_new_key(new_key_url, &key, &len, &get_key_url);
-  if (ret ==0) {
-    printf("new key succeeds, key len:%d, get_key_url:%s\n", len, get_key_url);
-    free(new_key_url);
-    free(get_key_url);
-    free(key);
+  if (!strcmp(argv[1], "newkey")) {
+    ret = oqkd_new_key(argv[2], &key, &len, &get_key_url);
+    if (ret != 0) {
+      printf("Fails to new key\n");
+      return -1;
+    } else {
+      printf("Key is:%s\n", key);
+      printf("GetkeyUrl:%s\n", get_key_url);
+      free(key);
+      free(get_key_url);
+      return 0;
+    }
+  } else if (!strcmp(argv[1], "getkey")) {
+    ret = oqkd_get_key(argv[2], &key, &len);
+    if (ret != 0) {
+      printf("Fails to get key\n");
+      return -1;
+    } else {
+      printf("Key is:%s\n", key);
+      free(key);
+      return 0;
+    }
   } else {
-    printf("Fails to new key\n");
+    printf("Usage:use the following two options to run\n");
+    printf("newkey newkeyurl\n");
+    printf("getkey getkeyurl\n");
+    return -1;
   }
-#else
-  /* run this on client side/alice/site C */
-  ret = oqkd_get_key("http://localhost:8095/api/getkey&siteid=B&index=0&blockid=f2febd3d-fcb4-4410-a522-66c672227d4c",
-      &key, &len);
-  if (ret == 0) {
-    printf("oqkd_get_key succeed: len=%d\n", len);
-    return 0;
-  } else {
-    return ret;
-  }
-#endif
-  return ret;
 }
