1041a26cfcf3eeb612a5b46b8ab1d74016ad4e5b
Kaiduan Xie <kaiduanx@goodstartsoft.com>
2026-08-24 11:58:37 -0400

    Add saeid to siteid converstion for ETSI 014 API.

--- kms/kms-service/src/main/java/com/uwaterloo/iqc/kms/controller/KMSController.java
+++ kms/kms-service/src/main/java/com/uwaterloo/iqc/kms/controller/KMSController.java
@@ -8,6 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import com.uwaterloo.iqc.kms.component.Key;
 import com.uwaterloo.iqc.kms.component.KeyPoolManager;
 import com.uwaterloo.iqc.kms.component.PolicyEngine;
@@ -22,7 +31,7 @@ public class KMSController {
     @Autowired private KeyPoolManager keyPoolMgr;
     @Autowired private PolicyEngine policy;
     private static final Logger logger = LoggerFactory.getLogger(KMSController.class);
-
+    private static final String URL = "http://127.0.0.1:9000/saeid2siteid.json";
     
     /**
      * Request a new key from KMS. KMS generating the key is considered
@@ -77,9 +86,10 @@ public class KMSController {
     //https://stackoverflow.com/questions/44839753/returning-json-object-as-response-in-spring-boot
     @RequestMapping("/v1/keys/{slaveSAEID}/enc_keys")
     public String getEncKey(@PathVariable String slaveSAEID) {
+        String siteID = getSiteIDFromSAEID(slaveSAEID, URL);
         Key k;
         if (policy.check()) {
-	    k = keyPoolMgr.newKey(slaveSAEID);
+	    k = keyPoolMgr.newKey(siteID);
 	    printKey(k, true);
         } else {
 	    k = new Key();
@@ -93,17 +103,18 @@ public class KMSController {
     @RequestMapping("/v1/keys/{slaveSAEID}/dec_keys")
     public String getDecKey(@PathVariable String slaveSAEID,
                             @RequestParam(value="key_ID") String keyID) {
+        String siteID = getSiteIDFromSAEID(slaveSAEID, URL);
         Key k;
         if (policy.check()) {
             // KeyID: index-blockId
             int i = keyID.indexOf('-');
             String index = keyID.substring(0, i);
             String blockId = keyID.substring(i + 1);
-            logger.info("slaveSAEID:" + slaveSAEID);
+            logger.info("siteID:" + siteID);
             logger.info("keyID:" + keyID);
             logger.info("index:" + index);
             logger.info("blockId:" + blockId);
-	    k = keyPoolMgr.getKey(slaveSAEID, blockId, Long.parseLong(index));
+	    k = keyPoolMgr.getKey(siteID, blockId, Long.parseLong(index));
 	    printKey(k, false);
         } else {
 	    k = new Key();
@@ -161,5 +172,39 @@ public class KMSController {
         String str = isNew ? "New Key:" : "Peer Key:";
         logger.info(str);
         logger.info(k.toString());
+    }
+
+    String getSiteIDFromSAEID(String SAEID, String url) {
+        if (SAEID == null) {
+            return "A";
+        }
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .header("Accept", "application/json")
+                .build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient().send(
+                request,
+                HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() < 200 || response.statusCode() >= 300) {
+                logger.error("HTTP Response:", response.statusCode());
+                return null;
+            }
+            logger.info("JSON result:" + response.body());
+            ObjectMapper om = new ObjectMapper();
+            JsonNode result = om.readTree(response.body());
+            if (result == null) {
+                logger.error("Fails to parse JSON:", response.body());
+                return null;
+            }
+            String siteId = result.get(SAEID).asText();
+            logger.info("SAEID:" + SAEID + "Site ID:" + siteId);
+            return siteId;
+        } catch (IOException |InterruptedException e) {
+            return null;
+        }
     }
 }
